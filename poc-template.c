d072d2ef6afadbbc7b8c80dc05c9952709d596c1
Thorsten Töpper <atsutane@freethoughts.de>
2025-04-08 21:57:55 +0200

    poc-template.c: macro for stderr fprintf with position in code

--- poc-template.c
+++ poc-template.c
@@ -7,6 +7,9 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <errno.h>
+
+#define LOGERR(...) {fprintf(stderr, "[%s:%d] %s: ", __FILE__, __LINE__, __func__); fprintf(stderr, __VA_ARGS__);}
 
 int main(int argc, char **argv) {
     return EXIT_SUCCESS;
